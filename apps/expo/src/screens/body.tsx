67130aa39ee07ce354e91f8115fbb08d19f6be8d
nguyen <nguyenkdksnb@gmail.com>
2023-03-27 17:51:12 +0700

    fix home

--- apps/expo/src/screens/body.tsx
+++ apps/expo/src/screens/body.tsx
@@ -1,17 +1,432 @@
-import { Button, Text } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "../root";
 import { ScreenProps } from "../types/navigation";
 
 export const BodyScreen = (props: ScreenProps<"Body">) => {
   return (
     <SafeAreaView
+      className="absolute"
       style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
     >
-      <Text>Body screen</Text>
-      <Button
-        title="Navigate"
-        onPress={() => props.navigation.navigate("Home")}
-      />
+      <ScrollView>
+        <View>
+          <TouchableOpacity className=" absolute inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <View className="inset-x-0 flex w-full flex-row space-x-3">
+              <View className="box-content rounded-2xl bg-white object-fill">
+                <Image
+                  className="m-2"
+                  source={require("../assets/images/home/neutral.png")}
+                />
+              </View>
+              <View className="inset-0flex-col relative flex  w-full justify-between">
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  DAILY CHECK IN
+                </Text>
+                <Text
+                  className="text-[#FFFFFF]"
+                  style={styles.textFontSemiBold}
+                >
+                  Hôm nay bạn cảm thấy thế nào?
+                </Text>
+                <Text className="text-[#DED5C6]" style={styles.textFont}>
+                  Tạm ổn &#8226; 2 ngày trước
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -104,12 +104,12 @@ export const HomeScreen = (props: ScreenProps<"Home">) => {
             />
           </TouchableOpacity>
         </View>
-        <View className="inset-x-0 mt-9 flex h-1/3 flex-col items-center justify-between">
+        <View className="inset-x-0 mt-9 flex flex-col items-center justify-between">
           <Image
             className=""
             source={require("../assets/images/home/center.png")}
           />
-          <View className="inset-x-0 mt-10 items-center">
+          <View className="inset-x-0 items-center">
             <Text
               className="text-2xl text-[#5A2D22]"
               style={styles.textFontSemiBold}
@@ -121,24 +121,24 @@ export const HomeScreen = (props: ScreenProps<"Home">) => {
             </Text>
           </View>
         </View>
-        <View className="relative bottom-0 right-0 left-0 -z-10 mt-10">
+        <View className="bottom-0 right-0 left-0 -z-10 ">
           <Image
-            className="absolute"
+            className="-mt-20"
             source={require("../assets/images/home/flowerCtr.png")}
           />
           <Image
-            className=" absolute mt-32 h-fit"
+            className=" absolute mt-5 h-fit"
             source={require("../assets/images/home/bottom.png")}
           />
 
-          <View className="absolute mt-60">
+          <View className="relative -mt-28">
             <Text
-              className=" pl-4 text-lg text-white"
+              className=" relative pl-4 text-lg text-white"
               style={styles.textFontSemiBold}
             >
               Thứ 2, ngày 1 tháng 12{" "}
             </Text>
-            <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+            <TouchableOpacity className=" inset-x-0 z-10 mx-2 mt-10">
               <View className="inset-x-0 flex w-full flex-row space-x-3">
                 <View className="box-content rounded-2xl bg-white object-fill">
                   <Image
@@ -162,6 +162,141 @@ export const HomeScreen = (props: ScreenProps<"Home">) => {
                 </View>
               </View>
             </TouchableOpacity>
+
+            <View>
+              <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+                <View className="inset-x-0 flex w-full flex-row space-x-3">
+                  <View className="box-content rounded-2xl bg-white object-fill">
+                    <Image
+                      className="m-2"
+                      source={require("../assets/images/home/neutral.png")}
+                    />
+                  </View>
+                  <View className="inset-0flex-col relative flex  w-full justify-between">
+                    <Text className="text-[#DED5C6]" style={styles.textFont}>
+                      DAILY CHECK IN
+                    </Text>
+                    <Text
+                      className="text-[#FFFFFF]"
+                      style={styles.textFontSemiBold}
+                    >
+                      Hôm nay bạn cảm thấy thế nào?
+                    </Text>
+                    <Text className="text-[#DED5C6]" style={styles.textFont}>
+                      Tạm ổn &#8226; 2 ngày trước
+                    </Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+                <View className="inset-x-0 flex w-full flex-row space-x-3">
+                  <View className="box-content rounded-2xl bg-white object-fill">
+                    <Image
+                      className="m-2"
+                      source={require("../assets/images/home/neutral.png")}
+                    />
+                  </View>
+                  <View className="inset-0flex-col relative flex  w-full justify-between">
+                    <Text className="text-[#DED5C6]" style={styles.textFont}>
+                      DAILY CHECK IN
+                    </Text>
+                    <Text
+                      className="text-[#FFFFFF]"
+                      style={styles.textFontSemiBold}
+                    >
+                      Hôm nay bạn cảm thấy thế nào?
+                    </Text>
+                    <Text className="text-[#DED5C6]" style={styles.textFont}>
+                      Tạm ổn &#8226; 2 ngày trước
+                    </Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+                <View className="inset-x-0 flex w-full flex-row space-x-3">
+                  <View className="box-content rounded-2xl bg-white object-fill">
+                    <Image
+                      className="m-2"
+                      source={require("../assets/images/home/neutral.png")}
+                    />
+                  </View>
+                  <View className="inset-0flex-col relative flex  w-full justify-between">
+                    <Text className="text-[#DED5C6]" style={styles.textFont}>
+                      DAILY CHECK IN
+                    </Text>
+                    <Text
+                      className="text-[#FFFFFF]"
+                      style={styles.textFontSemiBold}
+                    >
+                      Hôm nay bạn cảm thấy thế nào?
+                    </Text>
+                    <Text className="text-[#DED5C6]" style={styles.textFont}>
+                      Tạm ổn &#8226; 2 ngày trước
+                    </Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+                <View className="inset-x-0 flex w-full flex-row space-x-3">
+                  <View className="box-content rounded-2xl bg-white object-fill">
+                    <Image
+                      className="m-2"
+                      source={require("../assets/images/home/neutral.png")}
+                    />
+                  </View>
+                  <View className="inset-0flex-col relative flex  w-full justify-between">
+                    <Text className="text-[#DED5C6]" style={styles.textFont}>
+                      DAILY CHECK IN
+                    </Text>
+                    <Text
+                      className="text-[#FFFFFF]"
+                      style={styles.textFontSemiBold}
+                    >
+                      Hôm nay bạn cảm thấy thế nào?
+                    </Text>
+                    <Text className="text-[#DED5C6]" style={styles.textFont}>
+                      Tạm ổn &#8226; 2 ngày trước
+                    </Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
+                <View className="inset-x-0 flex w-full flex-row space-x-3">
+                  <View className="box-content rounded-2xl bg-white object-fill">
+                    <Image
+                      className="m-2"
+                      source={require("../assets/images/home/neutral.png")}
+                    />
+                  </View>
+                  <View className="inset-0flex-col relative flex  w-full justify-between">
+                    <Text className="text-[#DED5C6]" style={styles.textFont}>
+                      DAILY CHECK IN
+                    </Text>
+                    <Text
+                      className="text-[#FFFFFF]"
+                      style={styles.textFontSemiBold}
+                    >
+                      Hôm nay bạn cảm thấy thế nào?
+                    </Text>
+                    <Text className="text-[#DED5C6]" style={styles.textFont}>
+                      Tạm ổn &#8226; 2 ngày trước
+                    </Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
 
             <View>
               <TouchableOpacity className=" inset-x-0 mx-2 mt-10">
