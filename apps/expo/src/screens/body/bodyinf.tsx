5478d058022c79866034a35be34e83cf4f1ed392
Trang-K65ccuet <20020482@vnu.edu.vn>
2023-03-29 16:00:51 +0700

    add body

---
+++ apps/expo/src/screens/body/bodyinf.tsx
@@ -0,0 +1,52 @@
+import { Image, SafeAreaView, Text, View } from "react-native";
+
+export const BodyPage = () => {
+  return (
+    <SafeAreaView className="absolute inset-0 content-end bg-[#7A9861]">
+      <View className=" absolute top-0 left-0">
+        <Image
+          className="h-auto object-contain"
+          source={require("../../assets/images/body/body_page_header.png")}
+        />
+      </View>
+
+      <View className="absolute inset-x-0 mt-40 ml-auto mr-auto flex h-2/3 flex-col ">
+        <View className=" relative inset-x-0 mx-auto mt-10 w-full items-center">
+          <View className="absolute items-center justify-center">
+            <Image
+              className="object-fit w-auto items-center"
+              source={require("../../assets/images/body/logo.png")}
+            />
+
+            <Text className=" font-quicksand flex-wrap text-justify text-lg text-[#FFFF] ">
+              Một trải nghiệm studio ngay tại nhà
+            </Text>
+          </View>
+
+          <View className=" relative inset-x-0 mx-auto mt-10 w-3/4 items-center">
+            <Image
+              className=" relative mt-20 h-fit w-fit "
+              source={require("../../assets/images/body/center_image_body_page.png")}
+            />
+
+            <Text className=" font-quicksand mt-10 h-auto flex-wrap text-justify text-sm text-[#FFFF] ">
+              Bạn có thể tập luyện bất cứ đâu, bất cứ lúc nào theo các lớp tập
+              thú vị và hiệu quả của TGL. Mỗi tuần, TGL sẽ ra mắt 4 lớp tập mới
+              hoàn toàn, thân thiện cho người mới bắt đầu, áp dụng kỹ thuật và
+              phương pháp khoa học, để tạo dáng và săn gọn cơ thể người phụ nữ
+              một cách thích thú, an toàn, lâu dài. Hãy tập với TGL 4-7
+              ngày/tuần để có kết quả tốt nhất
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View className=" absolute inset-x-0 bottom-0 items-stretch">
+        <Image
+          className="h-fit w-fit object-cover"
+          source={require("../../assets/images/body/body_page_footer.png")}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
--- apps/expo/src/screens/body/nutrition.tsx
+++ apps/expo/src/screens/body/nutrition.tsx
@@ -8,43 +8,39 @@ import {
   Button,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { styles } from "../root";
-import { ScreenProps } from "../types/navigation";
+import { styles } from "../../root";
+import { ScreenProps } from "../../types/navigation";
 
-export const BodyScreen = (props: ScreenProps<"Body">) => {
+export const Nutrition = () => {
   return (
     <SafeAreaView className="absolute inset-0 content-end bg-[#FFF4ED]">
       <View className=" absolute top-0 left-0">
         <Image
           className="h-auto object-cover"
-          source={require("../assets/images/physique/header.png")}
+          source={require("../../assets/images/nutrition/header.png")}
         />
-        <TouchableOpacity
-          className="absolute top-20 right-10 p-1"
-          onPress={() => {}}
-        >
-          <Image source={require("../assets/images/physique/expand.png")} />
+        <TouchableOpacity className="absolute top-20 right-10 p-1">
+          <Image source={require("../../assets/images/physique/expand.png")} />
         </TouchableOpacity>
       </View>
       <View className="absolute inset-x-5 mt-40 ml-auto mr-auto flex flex-row">
         <TouchableOpacity>
-          <Text className=" font-quicksand text-2xl text-[#FF8669] ">
+          <Text className=" font-quicksand text-2xl text-[#9B9B9BE0] ">
             Thể chất
           </Text>
-          <View className=" absolute inset-x-0 mt-0">
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text className=" font-quicksand ml-10 text-2xl text-[#FF8669] ">
+            Dinh dưỡng
+          </Text>
+          <View className=" absolute inset-x-10 mt-0">
             <Image
               className="mt-10 h-auto object-cover"
-              source={require("../assets/images/physique/tab.png")}
+              source={require("../../assets/images/nutrition/tab.png")}
             />
           </View>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}}>
-          <Text className=" font-quicksand ml-10  text-2xl text-[#9B9B9BE0] ">
-            Dinh dưỡng
-          </Text>
-        </TouchableOpacity>
       </View>
-
       <View className="mt-10">
         <View
           className="ml-5 mr-5 mt-40 flex flex-row items-center border-2  border-[#FF835C33]/20 bg-[#FFF4ED] px-3 py-1"
@@ -52,13 +48,14 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
             borderRadius: 10,
           }}
         >
-          <Image source={require("../assets/images/physique/Icon.png")} />
+          <Image source={require("../../assets/images/nutrition/Icon.png")} />
           <TextInput
             placeholder="Tìm kiếm"
             className="px-2 py-1 text-sm text-[#9B9B9B]"
           />
         </View>
       </View>
+
       <View className="inset-x-5 mt-10 flex flex-row">
         <TouchableOpacity
           style={{
@@ -77,34 +74,6 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
           className="ml-5"
           style={{
             height: 40,
-            width: "15%",
-            borderRadius: 10,
-            backgroundColor: "#FFFFFF",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Text className=" font-quicksand text-sm text-[#A5A5A5] ">Yoga</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          className="ml-5"
-          style={{
-            height: 40,
-            width: "15%",
-            borderRadius: 10,
-            backgroundColor: "#FFFFFF",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Text className=" font-quicksand text-sm text-[#A5A5A5] ">Cadio</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          className="ml-5"
-          style={{
-            height: 40,
             width: "20%",
             borderRadius: 10,
             backgroundColor: "#FFFFFF",
@@ -113,7 +82,7 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
           }}
         >
           <Text className=" font-quicksand text-sm text-[#A5A5A5] ">
-            Pilates
+            Món chính
           </Text>
         </TouchableOpacity>
 
@@ -129,7 +98,23 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
           }}
         >
           <Text className=" font-quicksand text-sm text-[#A5A5A5] ">
-            Giãn cơ
+            Bữa sáng
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          className="ml-5"
+          style={{
+            height: 40,
+            width: "20%",
+            borderRadius: 10,
+            backgroundColor: "#FFFFFF",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text className=" font-quicksand text-sm text-[#A5A5A5] ">
+            Ăn nhẹ
           </Text>
         </TouchableOpacity>
       </View>
@@ -148,7 +133,7 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
               <View className="relative box-content">
                 <Image
                   className="h-40 w-40 rounded-lg object-fill"
-                  source={require("../assets/images/physique/img1.png")}
+                  source={require("../../assets/images/nutrition/img1.png")}
                 />
                 <View className="absolute bottom-2 left-4 rounded-2xl bg-slate-900/40 bg-opacity-10 p-1 ">
                   <Text className="text-white" style={styles.textFont}>
@@ -162,10 +147,12 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
                   className="text-base text-[#5A2D22]"
                   style={styles.textFont}
                 >
-                  Pilates xx
+                  Súp bí đỏ
                 </Text>
                 <View className=" flex flex-row items-baseline space-x-1">
-                  <Image source={require("../assets/images/home/heart.png")} />
+                  <Image
+                    source={require("../../assets/images/home/heart.png")}
+                  />
                   <Text className="text-xs text-[#DED5C6]">
                     22 &#8226; 11.02.2023{" "}
                   </Text>
@@ -177,7 +164,7 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
               <View className="relative box-content">
                 <Image
                   className="relative h-40 w-40 rounded-lg object-fill"
-                  source={require("../assets/images/physique/img2.png")}
+                  source={require("../../assets/images/nutrition/img2.png")}
                 />
                 <View className="absolute bottom-2 left-4 rounded-2xl bg-slate-900/40 bg-opacity-10 p-1 ">
                   <Text className="text-white" style={styles.textFont}>
@@ -191,10 +178,12 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
                   className="text-base text-[#5A2D22]"
                   style={styles.textFont}
                 >
-                  Giãn cơ 20 phút
+                  Sinh tố dâu
                 </Text>
                 <View className=" flex flex-row items-baseline space-x-1">
-                  <Image source={require("../assets/images/home/heart.png")} />
+                  <Image
+                    source={require("../../assets/images/home/heart.png")}
+                  />
                   <Text className="text-xs text-[#DED5C6]">
                     22 &#8226; 11.02.2023{" "}
                   </Text>
@@ -217,7 +206,7 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
               <View className="relative box-content">
                 <Image
                   className="h-40 w-40 rounded-lg object-fill"
-                  source={require("../assets/images/physique/img3.png")}
+                  source={require("../../assets/images/nutrition/img3.png")}
                 />
                 <View className="absolute bottom-2 left-4 rounded-2xl bg-slate-900/40 bg-opacity-10 p-1 ">
                   <Text className="text-white" style={styles.textFont}>
@@ -231,10 +220,12 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
                   className="text-base text-[#5A2D22]"
                   style={styles.textFont}
                 >
-                  Chạy
+                  Bánh plan
                 </Text>
                 <View className=" flex flex-row items-baseline space-x-1">
-                  <Image source={require("../assets/images/home/heart.png")} />
+                  <Image
+                    source={require("../../assets/images/home/heart.png")}
+                  />
                   <Text className="text-xs text-[#DED5C6]">
                     22 &#8226; 11.02.2023{" "}
                   </Text>
@@ -246,7 +237,7 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
               <View className="relative box-content">
                 <Image
                   className="relative h-40 w-40 rounded-lg object-fill"
-                  source={require("../assets/images/physique/img4.png")}
+                  source={require("../../assets/images/nutrition/img4.png")}
                 />
                 <View className="absolute bottom-2 left-4 rounded-2xl bg-slate-900/40 bg-opacity-10 p-1 ">
                   <Text className="text-white" style={styles.textFont}>
@@ -260,10 +251,12 @@ export const BodyScreen = (props: ScreenProps<"Body">) => {
                   className="text-base text-[#5A2D22]"
                   style={styles.textFont}
                 >
-                  Tập cơ tay
+                  Cốm
                 </Text>
                 <View className=" flex flex-row items-baseline space-x-1">
-                  <Image source={require("../assets/images/home/heart.png")} />
+                  <Image
+                    source={require("../../assets/images/home/heart.png")}
+                  />
                   <Text className="text-xs text-[#DED5C6]">
                     22 &#8226; 11.02.2023{" "}
                   </Text>
