411e22c76769cd27dc9d4681cb0addf75cddfbbc
Thinh Tran <thinhrom11@gmail.com>
2023-04-10 22:10:16 +0700

    chore: update music

--- apps/expo/src/assets/music/music.ts
+++ apps/expo/src/assets/music/music.ts
@@ -1,21 +1,51 @@
-const tracks = [
+const songs = [
   {
-    url: require("./Date 2.mp3"), // Load media from the network
-    title: "Avaritia",
-    artist: "deadmau5",
-    album: "while(1<2)",
-    genre: "Progressive House, Electro House",
-    date: "2014-05-20T07:00:00+00:00", // RFC 3339
-    artwork: "http://example.com/cover.png", // Load artwork from the network
-    duration: 402, // Duration in seconds
+    id: "1",
+    title: "Tình sầu",
+    album: "Em và Trịnh",
+    year: "2022",
+    url: require("./tinh-sau.mp3"),
+    imageUrl: require("./tinh-sau.jpg"),
   },
   {
-    url: require("./STAY.mp3"), // Load media from the app bundle
-    title: "Coelacanth I",
-    artist: "deadmau5",
-    artwork: require("./cover.jpg"), // Load artwork from the app bundle
-    duration: 166,
+    id: "2",
+    title: "The way that I love you",
+    album: "Songs for the Drunk and Broken Hearted",
+    year: "2021",
+    url: require("./the-way-that-i-love-you.mp3"),
+    imageUrl: require("./tinh-sau.jpg"),
+  },
+  {
+    id: "3",
+    title: "Người hãy quên em đi",
+    album: "Tâm 9",
+    year: "2017",
+    url: require("./nguoi-hay-quen-em-di.mp3"),
+    imageUrl: require("./tinh-sau.jpg"),
+  },
+  {
+    id: "4",
+    title: "Giữa đại lộ Đông Tây",
+    album: null,
+    year: "2021",
+    url: require("./giua-dai-lo-Dong-Tay.mp3"),
+    imageUrl: require("./tinh-sau.jpg"),
+  },
+  {
+    id: "5",
+    title: "Date 2",
+    album: "Your Name",
+    year: "2016",
+    url: require("./Date-2.mp3"),
+    imageUrl: require("./tinh-sau.jpg"),
+  },
+  {
+    id: "6",
+    title: "STAY",
+    album: "SQUARE TWO",
+    year: "2016",
+    url: require("./STAY.mp3"),
+    imageUrl: require("./tinh-sau.jpg"),
   },
 ];
-
-export default tracks;
+export default songs;
