1922f2acf0f9310ecf52aca2fcd3d98ef057f698
Trang-K65ccuet <20020482@vnu.edu.vn>
2023-05-07 21:35:16 +0700

    fix review page

--- apps/expo/src/screens/body/reviews.tsx
+++ apps/expo/src/screens/body/reviews.tsx
@@ -1,5 +1,28 @@
-import { Text, ScrollView, View, Image, TextInput } from "react-native";
+import {
+  Text,
+  ScrollView,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "../../root";
+
+const comments = [
+  {
+    name: "Julie Nguyen",
+    image: require("../../assets/images/nutrition/avatar.png"),
+    comment: "Yummy, thanks all ! memmm",
+    time: "2 giờ",
+  },
+  {
+    name: "Phuong Nguyen",
+    image: require("../../assets/images/nutrition/avatar.png"),
+    comment: "Ngon quá! Tôi sẽ làm thử",
+    time: "2 ngày",
+  },
+];
 
 export const ReviewsPage = () => {
   return (
@@ -12,7 +35,45 @@ export const ReviewsPage = () => {
             className="px-2 py-1 text-sm text-[#9B9B9B]"
           />
         </View>
-        <View className="absolute left-[5%] mt-80 h-40 w-[90%] justify-center rounded-xl bg-white"></View>
+        <View className=" mt-0">
+          {comments.map((item) => {
+            return (
+              <View className=" mt-5 ml-[5%] w-[90%] rounded-lg bg-white">
+                <View className="mt-3 ml-[5%] flex w-[90%] flex-row">
+                  <Image source={item.image} />
+                  <View className="ml-4">
+                    <Text
+                      className=" font-quicksand text-lg text-[#5A2D22] "
+                      style={styles().textFontSemiBold}
+                    >
+                      {item.name}
+                    </Text>
+                    <Text
+                      className=" font-quicksand text-sm text-[#3C3C43] "
+                      style={styles().textFont}
+                    >
+                      {item.time} trước
+                    </Text>
+                    <Text
+                      className=" font-quicksand mt-3 text-sm text-[#5A2D22] "
+                      style={styles().textFont}
+                    >
+                      {item.comment}
+                    </Text>
+                    <TouchableOpacity>
+                      <Text
+                        className=" font-quicksand mb-3 mt-3 text-sm text-[#FF835C] "
+                        style={styles().textFontSemiBold}
+                      >
+                        Trả lời
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            );
+          })}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
