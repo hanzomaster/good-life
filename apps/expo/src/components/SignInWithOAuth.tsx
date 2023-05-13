c85ebc0ff1cd37fbc1627744d0c17863a26c632c
Thinh Tran <thinhrom11@gmail.com>
2023-05-13 15:09:44 +0700

    feat(auth): sign in with Facebook

--- apps/expo/src/components/SignInWithOAuth.tsx
+++ apps/expo/src/components/SignInWithOAuth.tsx
@@ -139,6 +139,71 @@ const SignInWithOAuth = () => {
     }
   };
 
+  const handleSignInWithFacebookPress = async () => {
+    try {
+      const redirectUrl = AuthSession.makeRedirectUri({
+        path: "/oauth-native-callback",
+      });
+
+      await signIn.create({
+        strategy: "oauth_facebook",
+        redirectUrl,
+      });
+
+      const {
+        firstFactorVerification: { externalVerificationRedirectURL },
+      } = signIn;
+
+      if (!externalVerificationRedirectURL)
+        throw "Something went wrong during the OAuth flow. Try again.";
+
+      const authResult = await AuthSession.startAsync({
+        authUrl: externalVerificationRedirectURL.toString(),
+        returnUrl: redirectUrl,
+      });
+
+      if (authResult.type !== "success") {
+        throw "Something went wrong during the OAuth flow. Try again.";
+      }
+
+      // Get the rotatingTokenNonce from the redirect URL parameters
+      const { rotating_token_nonce: rotatingTokenNonce } = authResult.params;
+
+      await signIn.reload({ rotatingTokenNonce });
+
+      const { createdSessionId } = signIn;
+
+      if (createdSessionId) {
+        // If we have a createdSessionId, then auth was successful
+        await setSession(createdSessionId);
+      } else {
+        // If we have no createdSessionId, then this is a first time sign-in, so
+        // we should process this as a signUp instead
+        // Throw if we're not in the right state for creating a new user
+        if (
+          !signUp ||
+          signIn.firstFactorVerification.status !== "transferable"
+        ) {
+          throw "Something went wrong during the Sign up OAuth flow. Please ensure that all sign up requirements are met.";
+        }
+
+        console.log(
+          "Didn't have an account transferring, following through with new account sign up",
+        );
+
+        // Create user
+        await signUp.create({ transfer: true });
+        await signUp.reload({
+          rotatingTokenNonce: authResult.params.rotating_token_nonce,
+        });
+        await setSession(signUp.createdSessionId);
+      }
+    } catch (err) {
+      console.log(JSON.stringify(err, null, 2));
+      console.log("error signing in", err);
+    }
+  };
+
   return (
     <View className="flex flex-col gap-5">
       {/* <Button
@@ -153,7 +218,7 @@ const SignInWithOAuth = () => {
       <TouchableOpacity onPress={handleSignInWithGooglePress}>
         <Image source={require("../assets/images/signin/Google.png")} />
       </TouchableOpacity>
-      <TouchableOpacity onPress={handleSignInWithGithubPress}>
+      <TouchableOpacity onPress={handleSignInWithFacebookPress}>
         <Image source={require("../assets/images/signin/Facebook.png")} />
       </TouchableOpacity>
     </View>
